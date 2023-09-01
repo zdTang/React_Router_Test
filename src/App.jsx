f31df5a89d48add1aec567e8c887eab231726dc2
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-09-01 04:58:10 -0400

    3. Add Tailwind CSS to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home';
 import Profile from './Pages/Profile';
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div className="text-2xl text-center font-semibold mb-6">Profile</div>
   )
 }
