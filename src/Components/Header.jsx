de680297343a9deb88ad041dff9a37a36f7864d5
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-09-01 05:37:48 -0400

    4. Create basic menu in the Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='flex items-center justify-between max-w-6xl mx-auto my-5' >
+      <img className="h-5 cursor-pointer" src="images/logos/logo.svg"></img>
+      <ul className='flex space-x-10'>
+            <li>Home</li>
+            <li>Offers</li>
+            <li>Sign in</li>
+      </ul>
+
+    </div>
+    
   )
 }
