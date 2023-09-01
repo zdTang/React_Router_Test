7bb168e2779eee1250f76d2eb1c8a6bce4195fb7
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-31 22:32:07 -0400

    2. Create a Header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home';
 import Profile from './Pages/Profile';
+import Header from './Components/Header';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    
       <Router>
+      <Header/>{/* https://reactrouter.com/en/main/router-components/browser-router*/}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
