4e1fd299f1479b5948dd5e0c3086bf51e3ae8da1
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-09-01 06:01:52 -0400

    5. Track Location.   use useLocation to get URL path

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
+
+  const location = useLocation();
+  console.log(location);
+  console.log(location.pathname);
+
   return (
     <div className='flex items-center justify-between max-w-6xl mx-auto my-5' >
       <img className="h-5 cursor-pointer" src="images/logos/logo.svg"></img>
